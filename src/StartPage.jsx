7eb300c75772331e2af8fb2cc01043870ec43c7d
GeorgeTefnadze <giorgi.tefnadze.1@btu.edu.ge>
2025-01-09 00:42:39 +0400

    added shadcn library

---
+++ src/StartPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Button } from "@/components/ui/button";
+
+export default function StartPage() {
+  return (
+    <main className="bg-slate-400 w-screen h-screen flex flex-col content-center justify-center">
+      <div>StartPage</div>
+      <Button>Start</Button>
+    </main>
+  );
+}
