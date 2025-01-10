d5a96dfd5d3081b38ca1ee2a9cf3722f88b8e927
GeorgeTefnadze <giorgi.tefnadze.1@btu.edu.ge>
2025-01-11 00:26:16 +0400

    added firebase auth

--- firebase.js
+++ firebase.js
@@ -1,9 +1,6 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyBWYmWLvpxG3EUOC9QYApYIm27YGzRyl3o",
   authDomain: "word-bomb-a23fd.firebaseapp.com",
@@ -13,5 +10,7 @@ const firebaseConfig = {
   appId: "1:165336897223:web:7cbef7d6b6c8d1e6a1d0e2",
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export { app, auth };
--- src/RouterPage.jsx
+++ src/RouterPage.jsx
@@ -3,11 +3,14 @@ import { Route, Routes } from "react-router-dom";
 import App from "./App";
 import StartPage from "./StartPage";
 import LostPage from "./LostPage";
+import Login from "./components/auth/login";
+import Register from "./components/auth/register";
 
 export default function RouterPage() {
   return (
     <Routes>
-      <Route path="/" element={<StartPage />} />
+      <Route path="/" element={<Login />} />
+      <Route path="/start" element={<StartPage />} />
       <Route path="/game" element={<App />} />
       <Route path="/lost/:score" element={<LostPage />} />
     </Routes>
